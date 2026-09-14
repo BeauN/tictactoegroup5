b9af9fedb8fb49dea64cb219046b4ed9a066b949
Rana-from-Mars <51541362+Rana-from-Mars@users.noreply.github.com>
2026-09-14 12:44:09 -0700

    Update TicTacToe.java
    obliterated by old comments, can revert to an old version if theyre still needed

--- TicTacToe.java
+++ TicTacToe.java
@@ -140,39 +140,7 @@ public class TicTacToe {
             System.out.print("|");
         }
     }
-
-    /* here's what I think the rest of the class construction should look like I think? 
-    public void play() {
-        all the really annoying stuff
-    }
-    private void printStatus(int player);
-    private Status gameStatus() {
-         if (check if someone won) {
-            return Status.WIN;
-        }
-
-        if (check if board is full and no win ) {
-            return Status.DRAW;
-        }
-    
-        return Status.CONTINUE;
-    };
-    public void printBoard(){
-        for (int i = 0; i < BOARDSIZE; i++){
-            for (int j = 0; j < BOARDSIZE; j++){
-            and then it probably uses printSymbol(i, board[i][j]) or smth like that 
-            }
-        }
-    };
-    private void printSymbol(int column, char value){
-        GUYS IDK ON THIS ONE IM SORRY
-    }; 
-    private bool validMove(int row, int column){
-        return board[i][j] ==NULL;
-    };
-    
-    
-    */
+   
     public static void main(String[] args) {
 
         TicTacToe newGame = new TicTacToe();
